eb282eb40e5eef5b836967ce58026f68bb0abc83
Frida <fridapihlstrom@gmail.com>
2026-02-05 13:55:32 +0100

    feat: add sorting functionality

--- lab2-ramschema/src/js/main.js
+++ lab2-ramschema/src/js/main.js
@@ -2,6 +2,8 @@
 
 let allcourses = [];
 let filteredCourses = [];
+let lastSortedProperty = "";
+
 
 document.addEventListener("DOMContentLoaded", async () => {
     await getCourses();
@@ -18,8 +20,30 @@ document.addEventListener("DOMContentLoaded", async () => {
 
         displayCourses(filteredCourses);
     })
+
+    // funktion för sortering
+    function sortData(property) {
+        if (lastSortedProperty === property) {
+            filteredCourses.reverse()
+        } else {
+            filteredCourses.sort((a, b) => {
+                return a[property].localeCompare(b[property])
+            });
+
+            lastSortedProperty = property;
+        }
+
+        displayCourses(filteredCourses);
+    }
+
+    // händelselyssnare för sortering
+    document.querySelector("#sortCode").addEventListener("click", () => sortData("code"));
+    document.querySelector("#sortName").addEventListener("click", () => sortData("coursename"));
+    document.querySelector("#sortProg").addEventListener("click", () => sortData("progression"));
+    document.querySelector("#sortSyll").addEventListener("click", () => sortData("syllabus"));
 })
 
+// hämta JSON-data
 async function getCourses() {
     const url = "https://webbutveckling.miun.se/files/ramschema.json";
 
@@ -28,7 +52,9 @@ async function getCourses() {
         const response = await fetch(url);
         const courses = await response.json();
 
+        // lagra globalt, original och filtrerat
         allcourses = courses;
+        filteredCourses = [...courses];
 
         displayCourses(courses);
 
@@ -37,6 +63,7 @@ async function getCourses() {
     }
 }
 
+// skriver ut till DOM
 function displayCourses(data) {
     const tableEl = document.querySelector("#tableBody");
     tableEl.innerHTML = "";
@@ -47,7 +74,7 @@ function displayCourses(data) {
         <td class="coursecode">${course.code.toUpperCase()}</td>
         <td class="coursename">${course.coursename}</td>
         <td class="progression">${course.progression}</td>
-        <td class="syllabus"><a href="${course.syllabus}">Kursplan för ${course.coursename}</a></td>
+        <td class="syllabus"><a href="${course.syllabus}" target="_blank">Kursplan för ${course.coursename}</a></td>
         </tr>
         `
     })
